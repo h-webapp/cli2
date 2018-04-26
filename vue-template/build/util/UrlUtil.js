6255a304576c77fb2adab5d021280f21dde4c1ea
koujp <koujp@qq.com>
2018-04-26 09:54:02 +0800

    isAbsoluteUrl matched // prefix

--- vue-template/build/util/UrlUtil.js
+++ vue-template/build/util/UrlUtil.js
@@ -2,7 +2,7 @@ const path = require('path');
 const srcDir = require('./SrcDir');
 var contextDir = path.resolve(__dirname,'../../');
 function isAbsoluteUrl(src){
-    return /^((\/{1})*https*|file):\/\//i.test(src);
+    return /^(https*|file):\/{2}|^\/{2}/i.test(src);
 }
 function isNodeModuleUrl(src){
     return /\b\/?node_modules\b/.test(src);
