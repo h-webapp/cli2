d7c7fb24d994fc8655547e1b033f02248eb9108e
koujp <koujp@qq.com>
2018-04-27 09:53:42 +0800

    update version

--- vue-template/src/main/index.js
+++ vue-template/src/main/index.js
@@ -2,9 +2,6 @@ import { Module,Application,constant } from 'webapp-core';
 import Vue from 'vue';
 import Router from 'vue-router';
 Vue.use(Router);
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
-Vue.use(ElementUI);
 function main() {
     var routes = [];
     function buildRoute(routeOption){
